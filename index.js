4ef46b6b7ea882f3c72c9ca8fff0c418b0350649
Chineek Favors <chineekfavors01@gmail.com>
2020-01-17 20:41:46 -0800

    moved express middleware into own file named server.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
-const server = express();
+
+const server = require('./server.js');
 
 server.listen(4000, () =>{ console.log('server listening on port 4000')});
