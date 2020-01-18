6c1441784556ed9258b94d3ac9da4942729133ed
Chineek Favors <chineekfavors01@gmail.com>
2020-01-17 23:02:34 -0800

    created route for /:id/commits

--- data/dbRouter.js
+++ data/dbRouter.js
@@ -9,24 +9,36 @@ router.get('/', (req, res) => {
             res.status(200).json(post)
         })
         .catch(err => {
-            res.status(500).json({ error: "The posts information could not be retrieved.",})
+            res.status(500).json({ error: "The posts information could not be retrieved."})
         })
 });
 
 router.get('/:id', (req, res) => {
-    const {id} = req.params;
     
-    db.findById(id)
+    db.findById(req.params.id)
         .then(userId => {
-            if(id){
+            if(userId){
                 res.status(200).json(userId)
             } else{
                 res.status(404).json({message: "The post with the specified ID does not exist."})
-            }
-            
+            }    
         })
         .catch(err => {
-            res.status(500).json({ error: "The posts information could not be retrieved.",})
+            res.status(500).json({ error: "The posts information could not be retrieved."})
+        })
+});
+
+router.get('/:id/commits', (req, res) =>{
+    db.findCommentById(req.params.id)
+        .then(commitId => {
+            if(commitId){
+                res.status(200).json(commitId)
+            }else{
+                res.status(404).json({ error: "The comments information could not be retrieved." })
+            }
+        })
+        .catch(err => {
+            res.status(500)
         })
 })
 
